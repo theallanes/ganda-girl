f631a4a5057d079ed6e3d3abd48969fdcc213252
Thea <zllanes@academic.rrc.ca>
2025-04-06 23:36:50 -0500

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: "products#index"
+  resources :products, only: [ :index, :show ]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
