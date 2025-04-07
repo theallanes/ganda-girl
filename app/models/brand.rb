0a1dc1522cdf371f0d0a19ba47ea811fb2fcad03
Thea <zllanes@academic.rrc.ca>
2025-04-06 20:35:38 -0500

    Added ransackable attributes

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,2 +1,5 @@
 class Brand < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    [ "brand_name", "created_at", "id", "updated_at" ]
+  end
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,2 +1,5 @@
 class Type < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    [ "created_at", "id", "type_name", "updated_at" ]
+  end
 end
