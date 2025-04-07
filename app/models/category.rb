ed9eda3ed2d5596f0a9bb4756779cce16290c942
Thea <zllanes@academic.rrc.ca>
2025-04-06 20:38:30 -0500

    added ransackable to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ApplicationRecord
   belongs_to :type
+
+  def self.ransackable_associations(auth_object = nil)
+    [ "type" ]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "category_name", "created_at", "id", "type_id", "updated_at" ]
+  end
 end
