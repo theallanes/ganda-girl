5ce3718d3c725c82b0f46f1520ac4d28fbcdd079
Thea <zllanes@academic.rrc.ca>
2025-04-10 13:30:33 -0500

    added a file input for image

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,5 +1,14 @@
 ActiveAdmin.register Product do
-  permit_params :product_name, :description, :price, :image, :brand_id, :type_id
+  permit_params :product_name, :description, :price, :image, :brand_id, :type_id, :image
 
   remove_filter :image_attachment,:image_blob
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
