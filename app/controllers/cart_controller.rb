674588d711caa63072c9ed7fd00384759375c369
Thea <zllanes@academic.rrc.ca>
2025-04-16 16:39:17 -0500

    modified cart controller

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,7 +1,24 @@
 class CartController < ApplicationController
+  before_action :get_product, only: [ :create, :destroy ]
+
   def create
+    unless session[:cart].include?(@product.id)
+      session[:cart] << @product.id
+      flash[:notice] = "#{@product.product_name} was added to cart"
+    end
   end
 
   def destroy
+    if session[:cart].include?(@product.id)
+      session[:cart].delete(@product.id)
+      flash[:notice] = "#{@product.product_name} was removed from cart"
+    end
+  end
+
+  private
+
+  def get_product
+    product_id = params[:id].to_i
+    @product = Product.find(product_id)
   end
 end
